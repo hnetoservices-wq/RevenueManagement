0437bfa8677b1c0d98beaa454b20b2a38d520b8b
hnetoservices-wq <hnetoservices@gmail.com>
2026-09-22 18:19:34 +0100

    Initialize theme zoom and Portuguese UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { initPortugueseUi } from "./ui/ptPT";
+import { initUiPreferences } from "./ui/preferences";
 import "./styles.css";
 import "./snapshot.css";
+import "./ui/accessibility.css";
+
+initUiPreferences();
+initPortugueseUi();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
