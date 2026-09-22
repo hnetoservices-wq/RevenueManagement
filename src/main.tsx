7c553df223d9e7d1a6af25c4483c505d5e31b522
hnetoservices-wq <hnetoservices@gmail.com>
2026-09-22 19:31:35 +0100

    Load inventory localisation and theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { initPortuguesePhrases } from "./ui/ptPTPhrases";
 import { initPortugueseUi } from "./ui/ptPT";
+import { initPortugueseInventoryUi } from "./ui/ptPTInventory";
 import { initPortugueseNumberFormatting } from "./ui/ptPTNumbers";
 import { initUiPreferences } from "./ui/preferences";
 import "./styles.css";
 import "./snapshot.css";
 import "./ui/accessibility.css";
+import "./ui/inventoryTheme.css";
 
 initUiPreferences();
 initPortugueseNumberFormatting();
 initPortuguesePhrases();
 initPortugueseUi();
+initPortugueseInventoryUi();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
